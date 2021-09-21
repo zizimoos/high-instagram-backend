48247443fa82d7359c808700d378dabad3e4fe86
henry <henry@iMac-home.local>
2021-09-22 01:25:27 +0900

    prisma set up

---
+++ server.js
@@ -0,0 +1,42 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
+  }
+`;
+
+const resolvers = {
+  Query: {
+    movies: () => [],
+    movie: () => ({ title: "starwars", year: 1996 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(() => {
+  console.log(`✅  Server ready on http://localhost:4000/`);
+});
