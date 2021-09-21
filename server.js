049317c480e1bf6df590e6421f895b891f7a23ad
henry <henry@iMac-home.local>
2021-09-22 03:55:41 +0900

    prisma client part 1

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
+import { PrismaClient } from ".prisma/client";
 import { ApolloServer, gql } from "apollo-server";
+
+const client = new PrismaClient();
 
 const typeDefs = gql`
   type Movie {
-    title: String
-    year: Int
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
   type Query {
     movies: [Movie]
-    movie: Movie
+    movie(id: Int!): Movie
   }
   type Mutation {
-    createMovie(title: String!): Boolean
+    createMovie(title: String!, year: Int!, genre: String): Movie
     deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    movies: () => [],
-    movie: () => ({ title: "starwars", year: 1996 }),
+    movies: () => client.movie.findMany(),
+    movie: (_, { id }) => ({}),
   },
   Mutation: {
-    createMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
-    deleteMovie: (_, { title }) => {
-      console.log(title);
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: { title, year, genre },
+      }),
+    deleteMovie: (_, { id }) => {
       return true;
     },
   },
