88cb9250b9976d065dea1acc9a7300fec8d298e9
henry <henry@iMac-home.local>
2021-09-22 15:39:41 +0900

    prisma client part 2

--- server.js
+++ server.js
@@ -1,48 +1,13 @@
-import { PrismaClient } from ".prisma/client";
-import { ApolloServer, gql } from "apollo-server";
+import "dotenv/config";
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
 
-const client = new PrismaClient();
-
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    genre: String
-    createdAt: String!
-    updatedAt: String!
-  }
-  type Query {
-    movies: [Movie]
-    movie(id: Int!): Movie
-  }
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(title: String!): Boolean
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_, { id }) => ({}),
-  },
-  Mutation: {
-    createMovie: (_, { title, year, genre }) =>
-      client.movie.create({
-        data: { title, year, genre },
-      }),
-    deleteMovie: (_, { id }) => {
-      return true;
-    },
-  },
-};
+const PORT = process.env.PORT;
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
 });
 
-server.listen().then(() => {
-  console.log(`✅  Server ready on http://localhost:4000/`);
+server.listen(PORT).then(() => {
+  console.log(`✅  Server ready on http://localhost:${PORT}/`);
 });
